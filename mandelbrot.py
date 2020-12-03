05215ac39a53170bbe3be09748379c0a464ec98c
iion91 <52421433+iion91@users.noreply.github.com>
2020-12-03 16:14:19 +0100

    Add files via upload

---
+++ mandelbrot.py
@@ -0,0 +1,27 @@
+import numpy as np
+import numba
+import matplotlib.pyplot as plt
+
+def mandelbrot(Re, Im, max_iter):
+    c = complex(Re, Im)
+    z = 0.0j
+
+    for i in range(max_iter):
+        z= z*z +c
+        if(z.real*z.real + z.imag*z.imag) >= 4:
+            return i
+    return max_iter
+
+columns = 2000
+rows = 2000
+
+result = np.zeros([rows, columns])
+for row_index, Re in enumerate(np.linspace(-2, 1, num=rows)):
+    for column_index, Im in enumerate(np.linspace(-1, 1, num=columns)):
+        result[row_index, column_index] = mandelbrot(Re, Im, 100)
+
+plt.figure(dpi=100)
+plt.imshow(result.T, cmap='hot', interpolation='bilinear', extent=[-2, 1, -1, 1])
+plt.xlabel('Re')
+plt.ylabel('Im')
+plt.show()
